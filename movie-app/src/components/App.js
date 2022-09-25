84323cccec20ce08da70916ad82efe751fa30baf
yuvraj3905 <yuvraj202001@gmail.com>
2022-09-25 18:18:01 +0530

    Basic UI

---
+++ movie-app/src/components/App.js
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import {data} from '../data';
+import MovieCard from './MovieCard';
+import Navbar from './Navbar';
+
+function App() {
+  
+  return (
+    <div className="App">
+       <Navbar />
+       <div className="main">
+        <div className="tabs">
+          <div className="tab">
+           Movies
+          </div>
+          <div className="tab">
+            Favorites
+            </div>
+            </div>
+            <div className="list">
+              {data.map(movie =>(
+                <MovieCard movie={movie}/>
+              ))}
+              </div>
+        
+       </div>
+    </div>
+  );
+}
+
+
+export default App;
